cb91d333ffa0c36d2c2e3be0ca1a88e73c71ffe9
Sam Russell <sam.h.russell@gmail.com>
2017-04-22 21:08:49 +1200

    broken specs for next time

--- toppemtests/BgpMessageParserTests.cs
+++ toppemtests/BgpMessageParserTests.cs
@@ -11,14 +11,41 @@ namespace toppemtests
     public class BgpMessageParserTests
     {
         [Fact]
-        public void BgpMessageDecodesToTlv()
+        public void DecodeBgpKeepaliveMessage()
         {
-            var inputTlv = new Tlv(1, new byte[] { });
             var parser = new BgpMessageParser();
-            var bgpMessage = parser.Decode(inputTlv);
+            var keepAlive = new BgpKeepaliveMessage();
+            var tlv = new Tlv(3, new byte[] { });
+
+            Assert.Equal(keepAlive, parser.Decode(tlv));
+        }
+
+        [Fact]
+        public void EncodeBgpKeepaliveMessage()
+        {
+            var parser = new BgpMessageParser();
+            var keepAlive = new BgpKeepaliveMessage();
+            var tlv = new Tlv(3, new byte[] { });
+
+            Assert.Equal(tlv, parser.Encode(keepAlive));
+        }
+
+        [Fact]
+        public void BgpMessageEncodesToTlv()
+        {
+            // keepalive message
+            BgpMessageEncodeDecode(new Tlv(3, new byte[] { }));
+            // notification message "Unacceptable Hold Time"
+            BgpMessageEncodeDecode(new Tlv(4, new byte[] { 2, 6 }));
+        }
+
+        void BgpMessageEncodeDecode(Tlv tlv)
+        {
+            var parser = new BgpMessageParser();
+            var bgpMessage = parser.Decode(tlv);
             var outputTlv = parser.Encode(bgpMessage);
-            
-            Assert.Equal(inputTlv, outputTlv);
+
+            Assert.Equal(tlv, outputTlv);
         }
     }
 }
