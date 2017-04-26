dd9f29796fd60e722b2e44e8e0e8d2863fabdd44
Sam Russell <sam.h.russell@gmail.com>
2017-04-26 22:09:53 +1200

    self-packing subclasses

---
+++ toppem/Packable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toppem
+{
+    public class Packable
+    {
+        public byte[] Pack()
+        {
+            return GetType().GetFields().Select(field => PackField(field)).Aggregate((IEnumerable<byte>) new List<byte>(), (sum, next) => sum.Concat(next)).ToArray();
+        }
+
+        IEnumerable<byte> PackField(FieldInfo field)
+        {
+            dynamic data = field.GetValue(this);
+            return PackData(data);
+        }
+
+        IEnumerable<byte> PackData(byte data)
+        {
+            return new List<byte> { data };
+        }
+
+        IEnumerable<byte> PackData(short data)
+        {
+            return new List<int> { ((data >> 8) & 0xff), (data & 0xff) }.Select(x => Convert.ToByte(x));
+        }
+
+        IEnumerable<byte> PackData(int data)
+        {
+            return new List<int> {
+                ((data >> 24) & 0xff),
+                ((data >> 16) & 0xff),
+                ((data >> 8) & 0xff),
+                (data & 0xff)
+            }.Select(x => Convert.ToByte(x));
+        }
+    }
+}
---
+++ toppemtests/PackableTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using toppem;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace toppemtests
+{
+    public class PackableTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public PackableTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void SubclassesPackFields()
+        {
+            var subclass = new PackableSubclass(0xfe, 0x1234, 0x23456789);
+            Assert.Equal(new byte[] { 0xfe, 0x12, 0x34, 0x23, 0x45, 0x67, 0x89 }, subclass.Pack());
+        }
+    }
+
+    public class PackableSubclass : Packable
+    {
+        public byte i8;
+        public short i16;
+        public int i32;
+
+        public PackableSubclass(byte i8, short i16, int i32)
+        {
+            this.i8 = i8;
+            this.i16 = i16;
+            this.i32 = i32;
+        }
+    }
+}
